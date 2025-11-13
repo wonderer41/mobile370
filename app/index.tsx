6b16fd948944aaf294f63130ca3a7a0e7288d5b4
wonderer41 <gerardoo.41@gmail.com>
2025-11-13 16:41:52 -0700

    finished

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,11 @@
 import { Text, View} from "react-native";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "./global.css";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">this is a tet.</Text>
+      <Text className="text-3xl">this is a tezadsffdsft.</Text>
       <StatusBar style="auto"></StatusBar>
       <Link href="/profile" style={{color: "blue"}}>Go to profile</Link>
 
