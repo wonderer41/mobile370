7b9034b67f2acce4d373305349c637b758c25c47
wonderer41 <gerardoo.41@gmail.com>
2025-11-13 16:22:26 -0700

    finish addition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,12 @@
 import { Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import '../global.css';
 
 const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
-  );}
+  );
+}
 
-export default RootLayout
-
-const styles = StyleSheet.create({})
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View} from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">this is a tet.</Text>
+      <StatusBar style="auto"></StatusBar>
+      <Link href="/profile" style={{color: "blue"}}>Go to profile</Link>
+
     </View>
   );
 }
+
+
+
