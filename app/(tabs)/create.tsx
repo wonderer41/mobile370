228f662bb15a6c41d04f3c20e5476a0ec8ca45c5
wonderer41 <gerardoo.41@gmail.com>
2025-11-20 22:00:06 -0700

    add prfile page and bigining of create form

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Formfield from '../../components/Formfield'
 
 const Create = () => {
+
+  const [form , setForm ] = useState({
+    title: '',
+    video: null,
+    thumbnail: null,
+    prompt: ''
+  })
+
   return (
   <SafeAreaView className='bg-primary h-full'>
     <ScrollView className='px-4 my-6'>
       <Text className='text-white text-2xl text-white font-psemibold'>
         Upload Video
         </Text>
-      <Formfield>
-
-      </Formfield>
+      <Formfield
+        title="Video Title"
+      />
     </ScrollView>
 
   </SafeAreaView>
